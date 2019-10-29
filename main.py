f8602bf661dcd767ba4117a5913873b6b12671ec
OD <oddodaoddo@gmail.com>
2019-10-29 15:45:57 -0400

    Add remaining REST API points

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ import uvicorn
 from const import *
 from config import *
 from elastic import *
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import Range, Bool
 
-global host_ip, host_port
+global host_ip, host_port, es_client
 
 # get basic config
 db_host = get_db_host()
@@ -112,3 +114,73 @@ async def bedstat_serve(request:Request, id, format):
     else:
         return {'error': 'no data found'}
 
+from typing import List
+@app.get("/regionsets")
+async def bedstat_search(request:Request, filters:List[str] = Query(None)):
+
+    elastic_ops = {'<' : 'lt',
+                   '<=': 'lte',
+                   '=' : 'eq',
+                   '>' : 'gt',
+                   '>=': 'gte'}
+        
+    if filters:
+
+        # filters here looks something like (hopefully! e.g.):
+        # filters= ['nregions>=100', 'cpg<0.30', 'nregions=20']
+
+        # verify each filter is appropriate
+        # and attempt to aggregate comparison operations per filter
+        print('filters=', filters)
+        import re
+        re_pattern = '^(nregions|cpg)([<=>]+(=)*)(\d)+((\.)*(\d)+)'
+        filter_regex = re.compile(re_pattern)
+        filters_and_ops = {}
+        for f in filters:
+            result = filter_regex.match(f)
+            if not result:
+                return {"error" : "bad parameter --> " + f}
+            else:
+                # try to split on comparison op
+                s = re.split(r'([<=>]+(=)*)', f)
+                # element 0 = filter name (nregions or cpg)
+                # element 1 = comparison op (<, <=, =, >, >=)
+                # element 3 = numeric value (float or int)
+
+                # adjust query parameters to match json contents of database
+                if s[0] == 'nregions':
+                    sn = 'num_regions'
+                elif s[0] == 'cpg':
+                    sn = 'gc_content'
+                else:
+                    sn = s[0]
+
+                if sn in filters_and_ops:
+                    filters_and_ops[sn].append((elastic_ops.get(s[1]),s[3]))
+                else:
+                    filters_and_ops[sn] = [(elastic_ops.get(s[1]),s[3])]
+        # filters_and_ops here looks something like (e.g.):
+        # {'num_regions': [('gte', '100'), ('eq', '20')], 'gc_content': [('lt', '0.30')]}
+        print(filters_and_ops)
+        # build up the elastic query
+        should=[]
+        for filter_name in filters_and_ops:
+            val_list = filters_and_ops[filter_name]
+            for (fltr, op_val) in val_list:
+                # below approach is awkward since elasticsearch-dsl
+                # needs the actual name of the field in the Range object
+                # but NOT as string
+                if filter_name == 'num_regions':
+                    should.append(Range(num_regions={fltr : float(op_val)}))
+                elif filter_name == 'gc_content':
+                    should.append(Range(gc_content={fltr : float(op_val)}))
+                else:
+                    # there could be other filters down the road
+                    continue
+        s = Search(using=es_client).query(Bool(must=should))
+        response = s.execute()
+        if response.success():
+            return { "result" : response.to_dict()['hits']['hits'] }
+        else:
+            return { "result" : "no matching data found." }
+    return {"error": "no filters provided"}
