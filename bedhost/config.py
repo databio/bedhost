934a9e24a94cb35d462f1f8b4a14f40d80163e75
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-17 14:01:44 -0500

    add bed_index key option to cfg; cfg funcs enhancments

--- bedhost/config.py
+++ bedhost/config.py
@@ -1,20 +1,28 @@
-import os
 from .const import *
 
 
 def get_bedstat_base_path(cfg):
     if 'path_config' in cfg and 'bedstat_pipeline_output_path' in cfg['path_config']:
         return cfg['path_config']['bedstat_pipeline_output_path']
-    else:
-        return os.getcwd()
+    return os.getcwd()
 
 
 def get_db_host(cfg):
     if 'database' in cfg and 'host' in cfg['database']:
-        db_host = cfg['database']['host']
-    else:
-        db_host = 'localhost'
-    return db_host
+        return cfg['database']['host']
+    return 'localhost'
+
+
+def get_elastic_index_name(cfg):
+    """
+    Get the Elasticsearch index from config or return the default one
+
+    :param str cfg: path to the bedhost configuration file
+    :return str: Elasticsearch index name
+    """
+    if 'database' in cfg and 'bed_index' in cfg['database']:
+        return cfg['database']['bed_index']
+    return DEFAULT_BED_INDEX
 
 
 def get_server_cfg(cfg):
@@ -22,11 +30,11 @@ def get_server_cfg(cfg):
         if 'host' in cfg['server']:
             host_ip = cfg['server']['host']
         else:
-            host_ip = '0.0.0.0'
+            host_ip = DEFAULT_IP
         if 'port' in cfg['server']:
             host_port = cfg['server']['port']
         else:
-            host_port = 8000
+            host_port = DEFAULT_PORT
         return host_ip, host_port
     else:
-        return '0.0.0.0', DEFAULT_PORT
+        return DEFAULT_IP, DEFAULT_PORT
