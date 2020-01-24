cad8fc096cf8ae850ffb42c2b945e61554aafdf5
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-24 11:25:05 -0500

    change expected data type in AJAX post request

--- bedhost/static/queryBuilder.js
+++ bedhost/static/queryBuilder.js
@@ -2,7 +2,7 @@ var rules_basic = {
   rules: [{
     id: 'num_regions',
     operator: 'less',
-    value: 10
+    value: 100000
   }]
 };
 
@@ -24,20 +24,35 @@ $('#builder-basic').queryBuilder({
 
 $('#btn-reset').on('click', function() {
   $('#builder-basic').queryBuilder('reset');
+  $('#search-results').html("");
 });
 
-$('#btn-get-elastic').on('click', function() {
+$('#btn-show-elastic').on('click', function() {
   var result = $('#builder-basic').queryBuilder('getESBool');
-
-$.ajax({
-    url: "/bedfiles_filter_result",
-    type: "POST",
-    data: JSON.stringify(result),
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    });
 
   if (!$.isEmptyObject(result)) {
     alert(JSON.stringify(result, null, 2));
   }
 });
+
+$('#btn-post-elastic').on('click', function() {
+  var result = $('#builder-basic').queryBuilder('getESBool');
+
+var request = $.ajax({
+    url: "/bedfiles_filter_result",
+    type: "post",
+    data: JSON.stringify(result),
+    contentType: "application/json; charset=utf-8",
+    dataType: "html"
+    });
+request.done(function(data){
+      console.log("Data received: " + data);
+      $('#search-results').html(data);
+  });
+request.fail(function(jqXHR, textStatus){
+      console.log('Something went wrong: ' + textStatus);
+  });
+request.always(function(jqXHR, textStatus) {
+     console.log('Ajax request was finished')
+  });
+})
