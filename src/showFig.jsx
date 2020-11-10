79968d3bed498785929592ae68229f62707a386a
xuebingjie1990 <xuebingjie@hotmail.com>
2020-11-10 16:23:51 -0500

    fix img api path

--- src/showFig.jsx
+++ src/showFig.jsx
@@ -16,8 +16,8 @@ export default function ShowFig(props) {
       figList.push({
         id: i + 1,
         caption: props.bedNames[i],
-        src_pdf: bedhost_api_url + "/api/img/bedfiles/" + props.bedIds[i] + "/" + props.figType[0] + "/pdf",
-        src_png: bedhost_api_url + "/api/img/bedfiles/" + props.bedIds[i] + "/" + props.figType[0] + "/png"
+        src_pdf: bedhost_api_url + "/api/bed/" + props.bedIds[i] + "/img/" + props.figType[0] + "?format=pdf",
+        src_png: bedhost_api_url + "/api/bed/" + props.bedIds[i] + "/img/" + props.figType[0] + "?format=png"
       });
     }
   };
