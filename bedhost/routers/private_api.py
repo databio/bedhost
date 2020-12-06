14712661418cb4ec751a58e55da9be92c3439807
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-12-06 12:59:34 -0500

    start bbconf/pipestat support implementation

--- bedhost/routers/private_api.py
+++ bedhost/routers/private_api.py
@@ -35,12 +35,28 @@ async def get_query_results(
     if isinstance(query_val, str):
         query_val = [query_val]
     try:
-        return bbc.select(table_name=table_name, condition=query,
-                          condition_val=query_val, columns=columns)
+        return getattr(bbc, table_name).select(
+            condition=query, condition_val=query_val, columns=columns)
     except Exception as e:
         msg = f"Caught exception while querying the DB: {str(e)}"
         _LOGGER.error(msg)
         raise HTTPException(status_code=404, detail=msg)
+
+
+# @router.get("/filters/{table_name}")
+# async def get_search_setup_for_table(
+#         table_name: TableName = Path(
+#             ...,
+#             description="DB Table name"
+#         )
+# ):
+#     """
+#     Returns the filters mapping to based on the selected table schema to
+#     construct the queryBuilder to interface the DB
+#     """
+#     if table_name.value == BED_TABLE:
+#         return get_search_setup(bbc.get_bedfiles_table_columns_types())
+#     return get_search_setup(bbc.get_bedsets_table_columns_types())
 
 
 @router.get("/filters/{table_name}")
@@ -55,5 +71,5 @@ async def get_search_setup_for_table(
     construct the queryBuilder to interface the DB
     """
     if table_name.value == BED_TABLE:
-        return get_search_setup(bbc.get_bedfiles_table_columns_types())
-    return get_search_setup(bbc.get_bedsets_table_columns_types())
+        return get_search_setup(bbc.bed.schema)
+    return get_search_setup(bbc.bedset.schema)
