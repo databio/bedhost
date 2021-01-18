670311780938eadeb9ba482f3867150e2a1420b3
xuebingjie1990 <xuebingjie@hotmail.com>
2021-01-18 11:57:23 -0500

    fix bedhost_api_url

--- src/const.js
+++ src/const.js
@@ -4,4 +4,7 @@
 // const bedhost_api_url = bedhost_api_address + ":" + bedhost_api_port;
 // production mode
 const bedhost_api_url = `${ window.location.protocol }//${ window.location.host }`
+
+// const bedhost_api_url = 'http://dev1.bedbase.org'
+
 export default bedhost_api_url;
