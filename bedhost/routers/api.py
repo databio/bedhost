0d6f6cbd9ffd8c79641818246e9341173d2993eb
xuebingjie1990 <xuebingjie@hotmail.com>
2021-03-03 22:12:30 -0500

    code format

--- bedhost/routers/api.py
+++ bedhost/routers/api.py
@@ -76,7 +76,7 @@ async def get_file_for_bedfile(
         columns=["name", file_map_bed[id.value]],
     )[0][1]
     remote = True if bbc.config[CFG_PATH_KEY][CFG_REMOTE_URL_BASE_KEY] else False
-    #path = os.path.join(bbc.get_bedstat_output_path(remote), "..", "..", file["path"])
+    # path = os.path.join(bbc.get_bedstat_output_path(remote), "..", "..", file["path"])
     return serve_file(file["path"], remote)
 
 
