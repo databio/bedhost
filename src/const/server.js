9b844f5a73f3742ac47a68b13b0b74b80c46314f
Bingjie Xue <xuebingjie@hotmail.com>
2021-10-24 23:58:16 -0400

    update package.json

--- src/const/server.js
+++ src/const/server.js
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 // const bedhost_api_port = "8000";
 // const bedhost_api_url = bedhost_api_address + ":" + bedhost_api_port;
 
-// const bedhost_api_url = 'http://dev1.bedbase.org'
+//const bedhost_api_url = 'http://dev1.bedbase.org'
 
 // production mode
 const bedhost_api_url = `${window.location.protocol}//${window.location.host}`
