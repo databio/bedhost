8ba3cf7e45ace5608383128a6fea1a80213ee757
Bingjie Xue <xuebingjie@hotmail.com>
2022-07-29 14:33:52 -0400

    string search with fastapi endpoint 20ba81c32fd7a646f4ce7cca9e4d073675e89da1

--- src/searchResult.jsx
+++ src/searchResult.jsx
@@ -4,11 +4,14 @@ import Spinner from "react-bootstrap/Spinner";
 import { Paper } from "@material-ui/core";
 import { tableIcons } from "./tableIcons";
 import { Link } from "react-router-dom";
-import { client } from "./const/server";
 import { FaMinus } from "react-icons/fa";
 import { FaFolderPlus } from "react-icons/fa";
-import { GET_BED_DIST } from "./graphql/bedQueries";
-// import _ from "lodash";
+import axios from "axios";
+import bedhost_api_url from "./const/server";
+
+const api = axios.create({
+  baseURL: bedhost_api_url,
+});
 
 export default class ResultsBed extends React.Component {
   constructor(props) {
@@ -37,154 +40,38 @@ export default class ResultsBed extends React.Component {
   async getBedBySearchTerms() {
     let terms = this.props.terms.split(/[\s,]+/);
 
-    if (terms.length === 1) {
-      var data = await client
-        .query({
-          query: GET_BED_DIST,
-          variables: { filters: { searchTermIlike: terms[0] } },
-        })
-        .then(({ data }) => data.distances.edges);
+    var request = {
+      genome: this.props.genome,
+      terms: terms
+    };
 
-      var res = []
-      data.forEach((bed, index) => {
-        if (JSON.parse(bed.node.bedfile.genome).alias === this.props.genome) {
-          res.push(bed);
-        }
-      });
-      console.log(res)
-      res = res.slice().sort((a, b) => a.node.score - b.node.score);
-    } else {
-      var filter = []
-      terms.forEach((term, index) => {
-        filter.push({ searchTermIlike: term });
-      })
-      data = await client
-        .query({
-          query: GET_BED_DIST,
-          variables: { filters: { or: filter } },
-        })
-        .then(({ data }) => data.distances.edges);
+    let res = await api.post('/_private_api/distance/bedfiles/terms?ids=name&ids=md5sum&ids=other&ids=genome', request)
+      .then(({ data }) => data);
 
-      var res_all = []
-      data.forEach((bed, index) => {
-        if (JSON.parse(bed.node.bedfile.genome).alias === this.props.genome) {
-          res_all.push(bed);
-        }
-      });
-      var res_group = this.groupBy(res_all, "bedId");
+    console.log("test:", res)
 
-      res = [];
-      res_group.forEach((bed, index) => {
-        const thresh = 0.5
-        let avg_score = (bed.reduce((total, next) => total + next.node.score, 0) + thresh * (terms.length - bed.length)) / terms.length;
-        data = { ...bed[0].node, score: avg_score };
+    this.setState({
+      bedData: res,
+      terms: this.props.terms
+    });
 
-        delete data.searchTerm;
-        res.push({ node: data })
-      });
-
-      res = res.slice().sort((a, b) => a.node.score - b.node.score);
-      console.log(res)
-    }
-
-    this.setState({ bedData: res });
-    this.props.setSearchingFalse(false);
-
-    if (res.length >= 50) {
+    if (res.data.length >= 50) {
       this.setState({
         pageSize: 50,
         pageSizeOptions: [50, 100, 150],
       });
     } else {
       this.setState({
-        pageSize: res.length,
-        pageSizeOptions: [res.length],
+        pageSize: res.data.length,
+        pageSizeOptions: [res.data.length],
       });
     }
-    this.setState({ terms: this.props.terms });
+
+    this.props.setSearchingFalse(false);
     this.getColumns();
     this.getData();
   }
 
-  groupBy(arr, prop) {
-    const map = new Map(Array.from(arr, obj => [obj.node[prop], []]));
-    arr.forEach(obj => map.get(obj.node[prop]).push(obj));
-    return Array.from(map.values());
-  }
-
-  getUnique(arr1, arr2) {
-    var uniqueArr = []
-    for (var i = 0; i < arr1.length; i++) {
-      var flag = 0;
-      for (var j = 0; j < arr2.length; j++) {
-        if (arr1[i] === arr2[j]) {
-          arr2.splice(j, 1);
-          j--;
-          flag = 1;
-        }
-      }
-
-      if (flag === 0) {
-        uniqueArr.push(arr1[i]);
-      }
-    }
-    uniqueArr.push(arr2);
-    return uniqueArr;
-  }
-
-  // getAvgDist(old_res, new_res) {
-
-  //   const thresh = 0.5
-  //   var oldres = _.cloneDeep(old_res);
-  //   var newres = _.cloneDeep(new_res);
-  //   var avg_res = [];
-  //   var bed_old = old_res.map((bed, index) => {
-  //     return bed.node.bedId;
-  //   });
-  //   var bed_new = new_res.map((bed, index) => {
-  //     return bed.node.bedId;
-  //   });
-  //   const bedunique = this.getUnique(bed_old, bed_new)
-
-  //   avg_res = oldres.map((bed, index) => {
-  //     if (bedunique.includes(bed.node.bedId)) {
-  //       if (JSON.parse(bed.node.bedfile.genome).alias === this.props.genome) {
-  //         bed.node.score =
-  //           (bed.node.score + thresh) / 2;
-  //         return bed;
-  //       }
-  //     } else {
-
-  //       if (JSON.parse(bed.node.bedfile.genome).alias === this.props.genome) {
-  //         var new_res_idx = new_res.findIndex(function (new_bed) {
-  //           return new_bed.node.bedId === bed.node.bedId;
-  //         });
-
-  //         bed.node.score =
-  //           (bed.node.score + new_res[new_res_idx].node.score) / 2;
-
-  //         return bed;
-
-  //       }
-  //     }
-  //     return {}
-  //   });
-
-  //   newres.forEach((bed, index) => {
-  //     if (bedunique.includes(bed.node.bedId)) {
-  //       if (JSON.parse(bed.node.bedfile.genome).alias === this.props.genome) {
-  //         bed.node.score =
-  //           (bed.node.score + thresh) / 2;
-  //         avg_res.push(bed);
-  //       }
-  //     }
-  //   }
-  //   )
-
-  //   avg_res = avg_res.filter(value => Object.keys(value).length !== 0);
-
-  //   return avg_res;
-  // }
 
   getColumns() {
     let tableColumns = [];
@@ -264,14 +151,15 @@ export default class ResultsBed extends React.Component {
   }
 
   getData() {
-    let data = this.state.bedData.map((bed) => {
+    let data = this.state.bedData.data.map((bed) => {
+      let bf = {}
+      this.state.bedData.columns.forEach((key, i) => bf[key] = bed[i]);
       let row = {
-        id: bed.node.bedId,
-        name: bed.node.bedfile.name,
-        md5sum: bed.node.bedfile.md5sum,
-        relevance: this.getRelevance(bed.node.score),
+        name: bf.name,
+        md5sum: bf.md5sum,
+        relevance: this.getRelevance(bf.score),
       };
-      row = Object.assign({}, row, JSON.parse(bed.node.bedfile.other));
+      row = Object.assign({}, row, bf.other);
       return row;
     })
 
