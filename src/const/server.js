c8c825a2f680ac4cad6dbd9c469ebef0b5a4abfd
Bingjie Xue <xuebingjie@hotmail.com>
2023-09-01 08:28:15 -0400

    set server const for production mode

--- src/const/server.js
+++ src/const/server.js
@@ -1,9 +1,8 @@
 // dev mode
-const bedhost_api_address = "http://0.0.0.0";
-const bedhost_api_port = "8000";
-const bedhost_api_url = `${bedhost_api_address}:${bedhost_api_port}`;
-
-// const bedhost_api_url = 'http://bedbase.org'
+// const bedhost_api_address = "http://0.0.0.0";
+// const bedhost_api_port = "8000";
+// const bedhost_api_url = `${bedhost_api_address}:${bedhost_api_port}`;
+const bedhost_api_url = 'http://bedbase.org'
 
 // production mode
 // const bedhost_api_url = `${window.location.protocol}//${window.location.host}`
