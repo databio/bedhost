aae6ae504ae6b5db8602433ececa5e6d69945bb8
Bingjie Xue <xuebingjie@hotmail.com>
2023-09-01 14:46:58 -0400

    Update server.js

--- src/const/server.js
+++ src/const/server.js
@@ -2,7 +2,7 @@
 // const bedhost_api_address = "http://0.0.0.0";
 // const bedhost_api_port = "8000";
 // const bedhost_api_url = `${bedhost_api_address}:${bedhost_api_port}`;
-const bedhost_api_url = 'http://bedbase.org'
+const bedhost_api_url = 'https://bedbase.org'
 
 // production mode
 // const bedhost_api_url = `${window.location.protocol}//${window.location.host}`
