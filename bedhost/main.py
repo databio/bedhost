fa9d9137f3152614291e578d9f00adbd6e68c7c2
nsheff <nsheff@users.noreply.github.com>
2023-09-13 07:48:16 -0400

    update search return vals

--- bedhost/main.py
+++ bedhost/main.py
@@ -63,7 +63,12 @@ async def text_to_bed_search(
 ):
     _LOGGER.info(f"Searching for: {query}")
     _LOGGER.info(f"Using backend: {bbc.t2bsi}")
-    return bbc.t2bsi.nl_vec_search(query)
+    results = bbc.t2bsi.nl_vec_search(query, k=10)
+    for result in results:
+        # qdrant automatically adds hypens to the ids. remove them.
+        result["metadata"] = bbc.bed.retrieve(result['id'].replace('-', ''))
+        del result["vector"]  # no need to return the actual vectors
+    return results
 
 
 def attach_routers(app):
