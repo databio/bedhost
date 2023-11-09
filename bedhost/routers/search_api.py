0d1e7e7cc27c9e6bd5b956fa8ef5c6a07e89b40d
Khoroshevskyi <sasha99250@gmail.com>
2023-11-09 22:41:47 +0100

    updated bed search

--- bedhost/routers/search_api.py
+++ bedhost/routers/search_api.py
@@ -9,15 +9,15 @@ search_router = APIRouter(prefix="/search", tags=["search"])
 
 
 @search_router.get("/bed/{query}")
-async def text_to_bed_search(query):
+async def text_to_bed_search(query, number_of_results: int = 10):
     _LOGGER.info(f"Searching for: {query}")
     _LOGGER.info(f"Using backend: {bbc.t2bsi}")
-    results = bbc.t2bsi.nl_vec_search(query, k=10)
+    results = bbc.t2bsi.nl_vec_search(query, k=number_of_results)
     for result in results:
         del result["vector"]  # no need to return the actual vectors
         try:
             # qdrant automatically adds hypens to the ids. remove them.
-            result["metadata"] = bbc.bed.retrieve(result["id"].replace("-", ""))
+            result["metadata"] = bbc.bed.retrieve_one(result["id"].replace("-", ""))
         except RecordNotFoundError:
             _LOGGER.info(
                 f"Couldn't find qdrant result in bedbase for id: {result['id']}"
