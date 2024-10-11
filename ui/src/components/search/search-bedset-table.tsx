d2c54506918b24ce625dcdff7c7015c5000aab60
Marcel Schmager <marcel.schmager@deutschepost.de>
2024-10-11 18:50:39 +0200

    Add copy button to BEDsets

--- ui/src/components/search/search-bedset-table.tsx
+++ ui/src/components/search/search-bedset-table.tsx
@@ -1,44 +1,61 @@
 import { components } from '../../../bedbase-types';
+import { useCopyToClipboard } from '@uidotdev/usehooks';
+import {useState} from "react";
 
 type SearchResponse = components['schemas']['BedSetListResult'];
 
 type Props = {
-  results: SearchResponse;
+    results: SearchResponse;
 };
 
 export const SearchBedSetResultTable = (props: Props) => {
-  const { results } = props;
+    const { results } = props;
+    const [, copyToClipboard] = useCopyToClipboard();
+    const [copiedId, setCopiedId] = useState('');
 
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Bedset ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-          <th scope={'col'}>Number of Beds</th>
-          <th scope="col" style={{ minWidth: '140px' }}>
-            Actions
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {results.results?.map((result) => (
-          <tr key={result.id}>
-            <td>{result?.id || 'Unknown Id'}</td>
-            <td>{result?.name || 'Unknown Name'}</td>
-            <td>{result?.description || 'Unknown Description'}</td>
-            <td>{result?.bed_ids?.length || 0}</td>
-            <td>
-              <a className="me-1 align-content-center" href={`/bedset/${result?.id}`}>
-                <button className="btn btn-sm btn-outline-primary">
-                  <i className="bi bi-eye"></i>
-                </button>
-              </a>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    return (
+        <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">Bedset ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope={'col'}>Number of Beds</th>
+                <th scope="col" style={{ minWidth: '140px' }}>
+                    Actions
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            {results.results?.map((result) => (
+                <tr key={result.id}>
+                    <td>{result?.id || 'Unknown Id'}
+                        <button
+                            className="btn btn-link text-primary mb-2"
+                            onClick={() => {
+                                copyToClipboard(result?.id || '');
+                                setCopiedId(result?.id);
+                                setTimeout(() => {
+                                    setCopiedId('');
+                                }, 1000);
+                            }}
+                        >
+                            {copiedId === result.id ? <i className="bi bi-check me-1"/> : <i className="bi bi-clipboard me-1"/>}
+                        </button>
+                    </td>
+                    <td>{result?.name || 'Unknown Name'}</td>
+                    <td>{result?.description || 'Unknown Description'}</td>
+                    <td>{result?.bed_ids?.length || 0}</td>
+                    <td>
+                        <a className="me-1 align-content-center" href={`/bedset/${result?.id}`}>
+                            <button className="btn btn-sm btn-outline-primary">
+                                <i className="bi bi-eye"></i>
+                            </button>
+                        </a>
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
 };
