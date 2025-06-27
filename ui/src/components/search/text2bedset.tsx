e730596cfe068e39c300670529048b3b21eb25af
Khoroshevskyi <sasha99250@gmail.com>
2025-06-27 13:24:53 -0400

    fixed build

--- ui/src/components/search/text2bedset.tsx
+++ ui/src/components/search/text2bedset.tsx
@@ -15,7 +15,8 @@ export const Text2BedSet = () => {
   const [searchTerm, setSearchTerm] = useState(searchParams.get('q') || '');
   const [limit, setLimit] = useState(50);
   const [offset, setOffset] = useState(0);
-  const [genome, setGenome] = useState(searchParams.get('genome') || 'hg38');
+  const [genome, setGenome] = useState(searchParams.get('genome') || '');
+  const [assay, setAssay] = useState<string | undefined>(searchParams.get('assay') || undefined);
 
   const {
     isFetching: isSearching,
@@ -51,6 +52,8 @@ export const Text2BedSet = () => {
             setLimit={setLimit}
             value={searchTerm}
             genome={genome}
+            assay={assay}
+            setAssay={setAssay}
             setGenome={setGenome}
             onChange={setSearchTerm}
             onSearch={() => onSearch()}
