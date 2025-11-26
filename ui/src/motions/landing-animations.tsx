46f4f9d95a7368f41c5b57ff979247acbad05134
Khoroshevskyi <sasha99250@gmail.com>
2025-11-26 15:56:36 -0500

    polished animation

--- ui/src/motions/landing-animations.tsx
+++ ui/src/motions/landing-animations.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import { PRIMARY_COLOR } from '../const';
 
-const STROKE_WIDTH = 2;
+const STROKE_WIDTH = 2.5;
 const STROKE_SPEAD = 0;
 
 export const InPaths = () => {
